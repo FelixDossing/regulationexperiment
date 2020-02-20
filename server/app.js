ca19eebad8ac6e7bd0c3c0817d4bb97636de4cad
Felix Sebastian Døssing <drb625@ku.dk>
2020-02-20 14:07:47 +0100

    nriagn

--- server/app.js
+++ server/app.js
@@ -53,7 +53,7 @@ app.get('/', (req, res) => {
 
 app.get('*', (req, res) => {
     console.log(path.join(__dirname, '../client/index.html'))
-    res.sendFile(path.join(__dirname, 'client/index.html'));
+    res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
 // Start server
