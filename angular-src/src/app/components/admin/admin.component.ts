8f37e1785546a9f21c3f6ff49efe516439359ff6
Felix Sebastian Døssing <drb625@ku.dk>
2020-02-20 20:07:42 +0100

    commit

--- angular-src/src/app/components/admin/admin.component.ts
+++ angular-src/src/app/components/admin/admin.component.ts
@@ -77,36 +77,35 @@ export class AdminComponent implements OnInit {
   };
   getAdminData() {
     this.adminService.getData().subscribe(data => {
-      if (data) {
-        this.displayedColumns = ['session','first_name', 'last_name', 'email','role','register_date','instructions',
-        'allocation1','regulation1','allocation2','regulation2','survey1','survey2'];
-        this.dataSource = data.map(p => {
-          let info = { 
-                       _id:p._id,
-                       first_name : p.first_name,
-                       last_name: p.last_name,
-                       email:p.email,
-                       session:p.session,
-                       role:p.role,
-                       register_date:p.register_date.substr(0,10),
-                       instructions:p.tasks[0].completed ? 'Completed' : '-',
-                       allocation1:p.tasks[p.tasks.map(e=>e.task_tag).indexOf('allocation1')].completed ? 'Completed' : '-',
-                       allocation2:p.tasks[p.tasks.map(e=>e.task_tag).indexOf('allocation2')].completed ? 'Completed' : '-',
-                       regulation1:p.role=="worker" ? '-' : p.tasks[p.tasks.map(e => e.task_tag).indexOf('regulation1')].completed ? 'Completed' : '-',
-                       regulation2:p.role=="worker" ? '-' : p.tasks[p.tasks.map(e => e.task_tag).indexOf('regulation2')].completed ? 'Completed' : '-',
-                       survey1:p.tasks[p.tasks.map(e=>e.task_tag).indexOf('survey1')].completed ? 'Completed' : '-',
-                       survey2:p.tasks[p.tasks.map(e=>e.task_tag).indexOf('survey2')].completed ? 'Completed' : '-',
-                      }
-          return info;
-          });
-        if (this.dataview == 'reminder') {
-          this.dataSource = this.dataSource.filter(p => {
-            let signup_date = moment(p.register_date, 'YYYY-MM-DD');
-            let second_date = moment(signup_date).add({weeks:1, days:p.role == 'worker' ? 1 : 0});
-            let third_date = moment(second_date).add(1, 'weeks');
-            return this.today.isBetween(second_date, moment(second_date).add(1,'days')) || this.today.isBetween(third_date, moment(third_date).add(1,'days'))
-          });
-        }
+      console.log(data)
+      this.displayedColumns = ['session','first_name', 'last_name', 'email','role','register_date','instructions',
+      'allocation1','regulation1','allocation2','regulation2','survey1','survey2'];
+      this.dataSource = data.map(p => {
+        let info = { 
+                      _id:p._id,
+                      first_name : p.first_name,
+                      last_name: p.last_name,
+                      email:p.email,
+                      session:p.session,
+                      role:p.role,
+                      register_date:p.register_date.substr(0,10),
+                      instructions:p.tasks[0].completed ? 'Completed' : '-',
+                      allocation1:p.tasks.find(e => e.task_tag == 'allocation1').completed ? 'Completed' : '-',
+                      allocation2:p.tasks.find(e => e.task_tag == 'allocation2').completed ? 'Completed' : '-',
+                      regulation1:p.role=="worker" ? '-' : p.tasks[p.tasks.map(e => e.task_tag).indexOf('regulation1')].completed ? 'Completed' : '-',
+                      regulation2:p.role=="worker" ? '-' : p.tasks[p.tasks.map(e => e.task_tag).indexOf('regulation2')].completed ? 'Completed' : '-',
+                      survey1:p.tasks.find(e => e.task_tag == 'survey1').completed ? 'Completed' : '-',
+                      survey2:p.tasks.find(e => e.task_tag == 'survey2').completed ? 'Completed' : '-',
+                    }
+        return info;
+        });
+      if (this.dataview == 'reminder') {
+        this.dataSource = this.dataSource.filter(p => {
+          let signup_date = moment(p.register_date, 'YYYY-MM-DD');
+          let second_date = moment(signup_date).add({weeks:1, days:p.role == 'worker' ? 1 : 0});
+          let third_date = moment(second_date).add(1, 'weeks');
+          return this.today.isBetween(second_date, moment(second_date).add(1,'days')) || this.today.isBetween(third_date, moment(third_date).add(1,'days'))
+        });
       }
     })    
   }
