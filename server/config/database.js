aa275f6e843998d8c709ec781a5cbb0440609cad
Felix Sebastian Døssing <drb625@ku.dk>
2020-02-20 13:12:24 +0100

    commit

--- server/config/database.js
+++ server/config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/choice_limits_exp',
+    database: 'mongodb://<dbuser>:<dbpassword>@ds045795.mlab.com:45795/heroku_qw0jp4ts',
     secret: 'uarg84niwrf'
 }
