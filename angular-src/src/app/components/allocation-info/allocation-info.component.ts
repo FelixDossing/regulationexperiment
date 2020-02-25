55e1f8f0d219751ea734125fe3b98fa2dadcbb00
Felix Sebastian Døssing <drb625@ku.dk>
2020-02-25 13:10:26 +0100

    try agian

--- angular-src/src/app/components/allocation-info/allocation-info.component.ts
+++ angular-src/src/app/components/allocation-info/allocation-info.component.ts
@@ -10,9 +10,14 @@ export class AllocationInfoComponent implements OnInit {
   @Input() work_assignment:any;
   @Input() user:any;
 
+  reg_binding = false;
+
   constructor() { }
 
   ngOnInit() {
+    if (this.work_assignment && this.work_assignment.regulation_min > this.work_assignment.choice) {
+      this.reg_binding = true;
+    }
   }
 
 }
