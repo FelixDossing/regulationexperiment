3687d409f1f0c026e86f8a8bb12227f625357c1d
Felix Sebastian Døssing <drb625@ku.dk>
2020-03-11 12:32:54 +0100

    server

--- server/routes/users.js
+++ server/routes/users.js
@@ -198,6 +198,7 @@ router.post('/authenticate', (req, res, next) => {
 
 // Get profile
 router.get('/profile', passport.authenticate('jwt', {session:false}), (req, res) => {
+    console.log(req)
     res.json({user:req.user});
     // User.getUserByEmail(req.body.email, (err, dbUser) => {
     //     if (err) {
