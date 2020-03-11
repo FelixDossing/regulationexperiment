50f9e186fbef336cff7322a1d8be7e6a0b2d31a4
Felix Sebastian Døssing <drb625@ku.dk>
2020-03-11 12:20:59 +0100

    inprieng

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -49,6 +49,7 @@ export class AuthService {
   getProfile() {
     this.loadToken();
     let email = JSON.parse(localStorage.getItem('user')).email
+    console.log(email)
     let headers = new HttpHeaders().append('Content-Type','application/json').append('Authorization',this.authToken)
     return this.http.post<any>('users/profile', email, { headers:headers });
   }
