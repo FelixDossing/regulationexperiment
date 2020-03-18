cb59efd040dca75e24a26cbd6cf31cba724318c1
Felix Sebastian Døssing <drb625@ku.dk>
2020-03-18 01:25:50 +0100

    information box

--- server/routes/users.js
+++ server/routes/users.js
@@ -212,7 +212,6 @@ router.post('/profile', (req, res) => {
 // Complete instructions
 router.post('/completeinstructions', (req, res) => {
     let user = req.body;
-    console.log(user)
     user.tasks[0].completed = true;
     const info = { update_type: 'complete_instructions', user:user }
     User.updateUser(info, (err, response) => {
